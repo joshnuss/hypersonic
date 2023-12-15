f52a686325af2cb63d57dae879fa99b60059a9cf
Josh Nussbaum <joshnuss@gmail.com>
2023-12-14 22:29:56 -0500

    Switches editor to monaco

--- src/routes/Editor.svelte
+++ src/routes/Editor.svelte
@@ -3,9 +3,9 @@
   import { createClient } from '@liveblocks/client'
   import LiveblocksProvider from '@liveblocks/yjs'
   import * as Y from 'yjs'
-  import Quill from 'quill'
-  import { QuillBinding } from 'y-quill'
-  import QuillCursors from 'quill-cursors'
+
+  import { MonacoBinding } from 'y-monaco'
+  import * as monaco from 'monaco-editor'
 
   let element
 
@@ -22,28 +22,31 @@
 
     // Set up Yjs document, shared text, and Liveblocks Yjs provider
     const yDoc = new Y.Doc()
-    const yText = yDoc.getText('quill')
-    const yProvider = new LiveblocksProvider(room, yDoc)
+    const yText = yDoc.getText('text')
+    const provider = new LiveblocksProvider(room, yDoc)
 
-    // Attach cursors plugin
-    Quill.register('modules/cursors', QuillCursors)
-
-    // Set up Quill editor and modules
-    const quill = new Quill(element, {
-      placeholder: 'Start collaborating…',
-      theme: 'snow',
-      modules: {
-        cursors: true,
-        toolbar: [[{ header: [1, 2, false] }], ['bold', 'italic', 'underline'], ['code-block']],
-        history: {
-          // Local undo shouldn’t undo changes made by other users
-          userOnly: true
-        }
+    const editor = monaco.editor.create(element, {
+      value: '', // MonacoBinding overwrites this value with the content of type
+      theme: 'vs-dark',
+      language: 'markdown',
+      minimap: { enabled: false },
+      automaticLayout: true,
+      fontSize: 24,
+      scrollbar: {
+        vertical: 'auto',
+        horizontal: 'auto'
       }
     })
 
-    // Attach Yjs to Quill
-    const binding = new QuillBinding(yText, quill, yProvider.awareness)
+    editor.focus()
+
+    // Attach Yjs to Monaco editor
+    const binding = new MonacoBinding(
+      yText,
+      editor.getModel(),
+      new Set([editor]),
+      provider.awareness
+    )
 
     return () => {
       binding.destroy()
@@ -52,4 +55,33 @@
   })
 </script>
 
-<div bind:this={element}></div>
+<div id="editor" bind:this={element}></div>
+
+<style>
+  #editor {
+    width: 100%;
+    height: 100%;
+  }
+
+  :global(.yRemoteSelection) {
+    background-color: rgb(250, 129, 0, 0.5);
+  }
+
+  :global(.yRemoteSelectionHead) {
+    position: absolute;
+    border-left: orange solid 2px;
+    border-top: orange solid 2px;
+    border-bottom: orange solid 2px;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  :global(.yRemoteSelectionHead::after) {
+    position: absolute;
+    content: ' ';
+    border: 3px solid orange;
+    border-radius: 4px;
+    left: -4px;
+    top: -5px;
+  }
+</style>
