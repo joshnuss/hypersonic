6d716fa2f43326a410673118df2b5d7d2cdeeb8b
Josh Nussbaum <joshnuss@gmail.com>
2023-12-20 00:13:23 -0500

    Removes /docs prefix

--- src/routes/[...path].md/Editor.svelte
+++ src/routes/[...path].md/Editor.svelte
@@ -9,9 +9,6 @@
   import { initVimMode } from 'monaco-vim'
   import { MonacoMarkdownExtension } from 'monaco-markdown'
   import { marked } from 'marked'
-  import { baseUrl } from 'marked-base-url'
-
-  marked.use(baseUrl('/docs/'))
 
   export let mode = 'edit'
   export let roomName
