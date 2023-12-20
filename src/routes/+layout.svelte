6ee86ba2f33d8afbcd24291199c3828809bd3e51
Josh Nussbaum <joshnuss@gmail.com>
2023-12-20 01:06:49 -0500

    Adds fathom analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
   import '../app.css'
+  import { PUBLIC_FATHOM_SITE_CODE } from '$env/static/public'
 </script>
+
+{#if PUBLIC_FATHOM_SITE_CODE}
+  <script src="https://cdn.usefathom.com/script.js" data-site={PUBLIC_FATHOM_SITE_CODE} defer></script>
+{/if}
 
 <slot />
