a61330a53a0eceac3c404249e66153eef42b48b7
Josh Nussbaum <joshnuss@gmail.com>
2023-12-20 01:08:41 -0500

    Hides analytics in dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
   import '../app.css'
+  import { dev } from '$app/env'
   import { PUBLIC_FATHOM_SITE_CODE } from '$env/static/public'
 </script>
 
-{#if PUBLIC_FATHOM_SITE_CODE}
+{#if !dev && PUBLIC_FATHOM_SITE_CODE}
   <script src="https://cdn.usefathom.com/script.js" data-site={PUBLIC_FATHOM_SITE_CODE} defer></script>
 {/if}
 
