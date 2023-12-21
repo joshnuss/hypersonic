23579c1c3004e421661e006b1d57982b7f0c70c5
Josh Nussbaum <joshnuss@gmail.com>
2023-12-21 02:36:26 -0500

    Enable fathom analytics for SPA route changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,26 @@
 <script>
   import '../app.css'
-  import { dev } from '$app/environment'
+  import { onMount } from 'svelte'
+  import { dev, browser } from '$app/environment'
+  import { page } from '$app/stores'
   import { PUBLIC_FATHOM_SITE_CODE } from '$env/static/public'
+  import * as Fathom from 'fathom-client'
+
+  export let analyticsEnabled = !dev && PUBLIC_FATHOM_SITE_CODE
+
+  onMount(() => {
+    if (analyticsEnabled) {
+      Fathom.load(PUBLIC_FATHOM_SITE_CODE)
+    }
+  })
+
+  $: $page.url.pathname, browser && Fathom.trackPageview()
 </script>
 
-{#if !dev && PUBLIC_FATHOM_SITE_CODE}
+{#if analyticsEnabled}
   <script
     src="https://cdn.usefathom.com/script.js"
     data-site={PUBLIC_FATHOM_SITE_CODE}
-    data-spa="history"
     defer
   ></script>
 {/if}
