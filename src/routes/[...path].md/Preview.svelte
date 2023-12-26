5b056b9c88edc1305c8ba46a028f7dfbb1b04fab
Josh Nussbaum <joshnuss@gmail.com>
2023-12-26 05:11:52 -0500

    Move preview css into component

--- src/routes/[...path].md/Preview.svelte
+++ src/routes/[...path].md/Preview.svelte
@@ -10,3 +10,16 @@
 <div id="preview" class:visible={$mode == 'read'}>
   {@html html}
 </div>
+
+<style>
+  #preview {
+    height: 100%;
+    display: none;
+    margin: 0 3rem;
+    padding: 2rem 0;
+
+    &.visible {
+      display: block;
+    }
+  }
+</style>
