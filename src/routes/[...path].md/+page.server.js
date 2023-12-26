fdd017a5a6fb6521413cdf0ded76f159e5c498a8
Josh Nussbaum <joshnuss@gmail.com>
2023-12-26 01:18:31 -0500

    Ensure user is redirected to correct URL

--- src/routes/[...path].md/+page.server.js
+++ src/routes/[...path].md/+page.server.js
@@ -1,7 +1,14 @@
+import { redirect } from '@sveltejs/kit'
+
 export const ssr = false
 
-export function load({ params }) {
+export async function load({ url, params, locals }) {
   const path = params.path
+  const session = await locals.getSession()
+
+  if (!session?.user) {
+    throw redirect(303, `/auth/signin?callbackUrl=${url}`)
+  }
 
   return { path }
 }
