b309db6b343ecd4f06349061151c54d0e57aaa74
Josh Nussbaum <joshnuss@gmail.com>
2023-12-27 05:30:42 -0500

    Redirect to dashboard after login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,6 +22,6 @@
     <button on:click={() => signOut()} class="button">Sign out</button>
   {:else}
     <span class="notSignedInText">You are not signed in</span>
-    <button on:click={() => signIn('github')}>Sign In with GitHub</button>
+    <button on:click={() => signIn('github', { callbackUrl: '/dashboard' })}>Sign In with GitHub</button>
   {/if}
 </p>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Dashboard</h1>
+
+<a>Open recent</a>
