2ba80c26c5f4f97685e43663a608eab564723efd
Josh Nussbaum <joshnuss@gmail.com>
2023-12-27 06:11:27 -0500

    Displays MRU on dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,3 +1,25 @@
+<script>
+  import { onMount } from 'svelte'
+  import { getRecent } from '$lib/mru'
+
+  let recent
+
+  onMount(() => {
+    recent = getRecent()
+  })
+</script>
+
 <h1>Dashboard</h1>
 
-<a>Open recent</a>
+
+{#if recent}
+  <h2>Recent</h2>
+
+  <ul>
+    {#each recent as path}
+      <li>
+        <a href={path}>{path}</a>
+      </li>
+    {/each}
+  </ul>
+{/if}
