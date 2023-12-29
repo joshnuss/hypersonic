db24c24cbd0108ab8019bc107a043301ac84c1ff
Josh Nussbaum <joshnuss@gmail.com>
2023-12-29 11:36:00 -0500

    Simplifies main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-  import { signIn, signOut } from '@auth/sveltekit/client'
+  import { signIn } from '@auth/sveltekit/client'
   import { page } from '$app/stores'
-  import { user } from '$lib/user'
 </script>
 
 <h1>Hypersonic</h1>
@@ -10,21 +9,6 @@
 
 {JSON.stringify($page.data)}
 
-<p>
-  {#if $user}
-    {#if $user.image}
-      <span style="background-image: url('{$user.image}')" class="avatar" />
-    {/if}
-    <span class="signedInText">
-      <small>Signed in as</small>
-      <br />
-      <strong>{$user.name}</strong>
-    </span>
-    <button on:click={() => signOut()} class="button">Sign out</button>
-  {:else}
-    <span class="notSignedInText">You are not signed in</span>
-    <button on:click={() => signIn('github', { callbackUrl: '/dashboard' })}>
-      Sign In with GitHub
-    </button>
-  {/if}
-</p>
+<button on:click={() => signIn('github', { callbackUrl: '/dashboard' })}>
+  Sign In
+</button>
