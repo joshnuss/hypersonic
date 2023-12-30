b6da4fd23ce3a36c2d6702bc1378d02f607e076a
Josh Nussbaum <joshnuss@gmail.com>
2023-12-30 07:38:14 -0500

    Sorts by activity searching for file

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,7 +1,9 @@
+import { getRecent } from '$lib/usage.js'
 import Fuse from 'fuse.js'
 
 export function createSearch(list) {
-  const fuse = new Fuse(list, {
+  const sorted = sort(list)
+  const fuse = new Fuse(sorted, {
     // isCaseSensitive: false,
     // includeScore: false,
     // shouldSort: true,
@@ -20,9 +22,32 @@ export function createSearch(list) {
 
   return {
     filter(pattern) {
-      if (!pattern) return list
+      if (!pattern) return sorted
 
       return fuse.search(pattern).map((result) => result.item)
     }
   }
+}
+
+function sort(list) {
+  if (list.length == 0) return []
+
+  const recentPaths = getRecent()
+  const recentFiles = new Array(recentPaths.length)
+  const rest = []
+
+  list.forEach((file) => {
+    const index = recentPaths.indexOf(file.path)
+
+    if (index > -1) {
+      recentFiles[index] = file
+    } else {
+      rest.push(file)
+    }
+  })
+
+  return [
+    ...recentFiles,
+    ...rest
+  ]
 }
