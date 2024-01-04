9bb43235e68ffc367f23e4c8db3e37fe7e2acd4d
Josh Nussbaum <joshnuss@gmail.com>
2024-01-04 17:17:38 -0500

    Re-opens last page when available

---
+++ src/routes/open/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { goto } from '$app/navigation'
+  import { onMount } from 'svelte'
+  import { getRecent } from '$lib/usage'
+  import Loader from '$lib/components/Loader.svelte'
+
+  onMount(() => {
+    const paths = getRecent()
+
+    if (paths.length > 0) {
+      goto(paths[0])
+    } else {
+      goto('/welcome.md')
+    }
+  })
+</script>
+
+<Loader/>
